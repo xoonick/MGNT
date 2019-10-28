e250907d6f1fb79c4a8c4f8bcdd86e37e43c072f
Nick Tuschek <Nick.Tuschek@KNO_VA.de>
2019-10-28 09:12:50 +0100

    Ordnerstruktur

---
+++ MGNT/Backend/DB_Anbindung.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * DB_Anbindung short summary.
+ *
+ * DB_Anbindung description.
+ *
+ * @version 1.0
+ * @author U1349
+ */
+class DB_Anbindung
+{
+}
---
+++ MGNT/Frontend/LoginGUI.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * LoginGUI short summary.
+ *
+ * LoginGUI description.
+ *
+ * @version 1.0
+ * @author U1349
+ */
+class LoginGUI
+{
+}
