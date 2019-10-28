03f924edcd25c36180d8681d2a4b979fcfe33925
Nick Tuschek <Nick.Tuschek@KNO_VA.de>
2019-10-28 10:23:44 +0100

    Insert, ID of last Insert, check DB connection

--- MGNT/Backend/DB_Anbindung.php
+++ MGNT/Backend/DB_Anbindung.php
@@ -4,10 +4,57 @@
  * DB_Anbindung short summary.
  *
  * DB_Anbindung description.
+ * 
+ * Querys and any other 
  *
  * @version 1.0
  * @author U1349
  */
-class DB_Anbindung
-{
+
+error_reporting(E_ALL);
+
+define('MYSQL_HOST', 'localhost');
+
+define('MYSQL_BENUTZER', 'root');
+define('MYSQL_KENNWORT', '');
+
+// 2 parameter entspricht dem Tabellennamen
+define('MYSQL_DATENBANK', 'stempeluhr');
+
+$db_link = mysqli_connect(
+                MySQL_HOST,
+                MYSQL_BENUTZER,
+                MYSQL_KENNWORT,
+                MYSQL_DATENBANK
+          );
+
+// check the DB connection 
+function checkConnection() {
+    global $db_link;
+
+    if($db_link) {
+        echo 'Verindung erfolgreich';
+    } else {
+        die('Keine Verbindung möglich: ' .mysqli_error($db_link));
+    }
+}
+
+// Insert
+function insertInto() {
+    global $db_link;
+
+    $sql_insert = "INSERT INTO `TABELLE` (`SPALTE`) VALUES (`WERTE`)";
+    $resultInsert = $db_link->query($sql_insert)
+        or die("Anfrage fehlgeschlagen: " .mysqli_error($db_link));
+}
+
+// Get ID from last insert
+function getIdOfLastInsert() {
+    global $db_link;
+
+    $sql_select_last_insert_id = "SELECT ID FROM `TABELLE` ORDER BY DESC LIMIT 1";
+    $resultLastInsertId = $db_link->query($sql_select_last_insert_id)
+        or die("Anfrage fehlgeschlagen: " .mysqli_error($db_link));
+    $tempVar = $resultLastInsertId->fetch_object();
+    $lastId = $tempVar->ID;
 }
